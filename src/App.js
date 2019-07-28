a24f2994d92d2dfdbea32d8fe184577b22048228
Erzhuyijian <1821344102@qq.com>
2019-07-28 16:08:53 +0800

    添加 Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import Header from './component/Header';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <Button type="primary">Button</Button>
+          <Header />
         </div>
     );
   }
