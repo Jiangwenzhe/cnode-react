4bb3ee4e69c18c9189f79a2166d6643e39dd4424
Erzhuyijian <1821344102@qq.com>
2019-07-28 18:22:27 +0800

    将 home 组件通过 router的方式加入到 App.js上

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Redirect } from 'react-router-dom'
 import Header from './component/Header';
+import Home from './pages/Home';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
+        <div>
           <Header />
+          <HashRouter>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/topic" render={() => <Redirect to="/" />} />
+            </div>
+          </HashRouter>
         </div>
     );
   }
