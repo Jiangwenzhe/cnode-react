772b8b52e740844537e4d78ab86c6454696e7212
Erzhuyijian <1821344102@qq.com>
2019-07-29 10:53:21 +0800

    Home 组件添加 tabs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import { Tabs } from 'antd';
 import './home.scss';
 import mock_data from '../../assets/mock-data/home';
+
+const { TabPane } = Tabs;
 
 class Home extends Component {
   constructor() {
     super();
   }
 
+  handleTabChange = tab => {
+    console.log(tab);
+  };
+
   render() {
     console.log(mock_data);
     return (
-      <div>
-        home
+      <div className="home">
+        <Tabs defaultActiveKey="1" onChange={this.handleTabChange}>
+          <TabPane tab="全部" key="1">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="精华" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="分享" key="3">
+            Content of Tab Pane 3
+          </TabPane>
+          <TabPane tab="问答" key="4">
+            Content of Tab Pane 4
+          </TabPane>
+          <TabPane tab="招聘" key="5">
+            Content of Tab Pane 5
+          </TabPane>
+        </Tabs>
       </div>
     )
   }
