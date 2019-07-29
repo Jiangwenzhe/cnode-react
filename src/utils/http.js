4dbf5785fd3aa5c89593212d9552b97f21dfe34b
Erzhuyijian <1821344102@qq.com>
2019-07-29 20:51:30 +0800

    添加有关ajax请求的公用方法

---
+++ src/utils/http.js
@@ -0,0 +1,38 @@
+import { message } from "antd";
+import axios from 'axios';
+
+// 实现ajax接口的拦截
+
+const error = (errorMessage) => {
+  message.error(`${errorMessage}`, 1);
+};
+
+// 创建一个axios对象
+const http = axios.create({
+  baseURL: 'https://cnodejs.org/api/v1',
+  timeout: 5000
+});
+
+// 发送请求拦截器
+http.interceptors.request.use(
+  config => {
+    return config;
+  },
+  err => {
+    error('发送请求错误!');
+    return Promise.error(err);
+  }
+);
+
+// 接受请求拦截器
+http.interceptors.request.use(
+  response => {
+    return response.data;
+  },
+  err => {
+    error(`返回请求错误，响应码 ${err.response.status}`);
+    return Promise.error(error);
+  }
+);
+
+export default http;
