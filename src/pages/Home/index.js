f4a8d569af5ea159e74c8aed2ba6814b75b3ba95
Erzhuyijian <1821344102@qq.com>
2019-07-30 14:10:56 +0800

    添加更换 tab 显示不同内容的功能

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,89 @@
 import React, { Component } from 'react';
 import { Tabs } from 'antd';
 import './home.scss';
-import mock_data from '../../assets/mock-data/home';
+import { getTopics } from "../../utils/api";
+import TopicLists from "../../component/TopicLists";
 
 const { TabPane } = Tabs;
 
 class Home extends Component {
-  constructor() {
-    super();
-  }
+  state = {
+    page: 1,
+    limit: 30,
+    topicInfo: [],
+    tab: 'all',
+    store: {},
+  };
+
+  getTopicsInfo() {
+    const { page, limit, tab, store } = this.state;
+    getTopics({
+      page: page,
+      limit: limit,
+      tab: tab
+    }).then(response => {
+      this.setState({
+        topicInfo: response.data,
+        page: page + 1
+      });
+      store[tab] = {
+        page: page,
+        data: response.data,
+      }
+    });
+  };
 
   handleTabChange = tab => {
     console.log(tab);
+    const { store } = this.state;
+    if (!store[tab]) {
+      this.setState(
+        {
+          tab,
+          page: 1,
+          topicInfo: []
+        },
+        () => {
+          this.getTopicsInfo();
+        }
+      );
+      return;
+    }
+    this.setState({
+      tab,
+      page: store[tab].page,
+      topicInfo: store[tab].data
+    })
   };
 
+  componentDidMount() {
+    this.getTopicsInfo();
+  }
+
   render() {
-    console.log(mock_data);
+    const { topicInfo } = this.state;
     return (
       <div className="home">
-        <Tabs defaultActiveKey="1" onChange={this.handleTabChange}>
-          <TabPane tab="全部" key="1">
-            Content of Tab Pane 1
-          </TabPane>
-          <TabPane tab="精华" key="2">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane tab="分享" key="3">
-            Content of Tab Pane 3
-          </TabPane>
-          <TabPane tab="问答" key="4">
-            Content of Tab Pane 4
-          </TabPane>
-          <TabPane tab="招聘" key="5">
-            Content of Tab Pane 5
-          </TabPane>
-        </Tabs>
+        {/*<Loading status={true}/>*/}
+        <div>
+          <Tabs defaultActiveKey="all" onChange={this.handleTabChange}>
+            <TabPane tab="全部" key="all">
+              <TopicLists topicInfo={topicInfo}/>
+            </TabPane>
+            <TabPane tab="精华" key="good">
+              <TopicLists topicInfo={topicInfo}/>
+            </TabPane>
+            <TabPane tab="分享" key="share">
+              <TopicLists topicInfo={topicInfo}/>
+            </TabPane>
+            <TabPane tab="问答" key="ask">
+              <TopicLists topicInfo={topicInfo}/>
+            </TabPane>
+            <TabPane tab="招聘" key="job">
+              <TopicLists topicInfo={topicInfo}/>
+            </TabPane>
+          </Tabs>
+        </div>
       </div>
     )
   }
